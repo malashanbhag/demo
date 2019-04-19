4c2bc8729b8eb80df1ebf3e55f66310862dd0f59
Mala <shanbhag.mala9@gmail.com>
2019-04-19 20:40:26 +0530

    styling changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import './Person/Person.css'
+import './Person/Person.css'; 
 
 import Person from './Person/Person';
 
@@ -42,11 +42,12 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor : 'white',
+      backgroundColor : 'green',
+      color : 'white',
       fint: 'inherit',
       border: '1px solid blue',
       padding : '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
     };
 
     let persons = null;
@@ -64,18 +65,29 @@ class App extends Component {
           })}
           </div>
       );
+
+      style.backgroundColor = 'red';
+    }
+
+    let classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red')
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold')
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm React App!!</h1>
-        <button
-          style={style} 
-          onClick={this.togglePersonsHandler}> 
-            Toggle Persons
-        </button>
-        {persons}
-      </div>
+        <div className="App">
+          <h1>Hi, I'm React App!!</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button
+            style={style} 
+            onClick={this.togglePersonsHandler}> 
+              Toggle Persons
+          </button>
+          {persons}
+        </div>
     );
   }
 }
